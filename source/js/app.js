1e51ab973f26f30b22953ee98fc9011171d46ab8
sunaiwen <sunivengg@gmail.com>
2015-05-23 01:10:17 +0800

    modify zoomerange.js and add image preview support

--- source/js/app.js
+++ source/js/app.js
@@ -1,56 +1,71 @@
-(function(){
-    window.o = {};
-})();
+;(function(){
+    window.o = {}
+})()
 
-(function(win, doc){
+// search control
+;(function(win, doc){
     var Search = function(){
-        this.wrap = doc.getElementById('j-searchBox');
-        this.openBtn = doc.getElementById('j-nav-searchBtn');
-        this.closeBtn = doc.getElementById('j-search-closeBtn');
+        this.wrap = doc.getElementById('j-searchBox')
+        this.openBtn = doc.getElementById('j-nav-searchBtn')
+        this.closeBtn = doc.getElementById('j-search-closeBtn')
 
-        this.bindEvent();
-    };
+        this.bindEvent()
+    }
 
     Search.prototype.bindEvent = function(){
         if(this.wrap.style.display === 'none') {
-            return;
+            return
         }
 
-        var self = this;
-        var openBtn = this.openBtn;
-        var closeBtn = this.closeBtn;
+        var self = this
+        var openBtn = this.openBtn
+        var closeBtn = this.closeBtn
 
         openBtn.addEventListener('click', function(){
-            self.show();
-        }, false);
+            self.show()
+        }, false)
         closeBtn.addEventListener('click', function(){
-            self.hide();
-        }, false);
-    };
+            self.hide()
+        }, false)
+    }
 
     Search.prototype.show = function(){
-        this.wrap.className += (' ' + 'is-show');
-    };
+        this.wrap.className += (' ' + 'is-show')
+    }
 
     Search.prototype.hide = function(){
-        var className = this.wrap.className;
-        var index = className.indexOf('is-show');
+        var className = this.wrap.className
+        var index = className.indexOf('is-show')
 
         if(index > 0) {
-            className = className.slice(0, index - 1);
+            className = className.slice(0, index - 1)
         } else {
-            className = '';
+            className = ''
         }
 
-        this.wrap.className = className;
-    };
+        this.wrap.className = className
+    }
 
-    win.o.Search = Search;
-})(window, document);
+    win.o.Search = Search
+})(window, document)
 
-(function(win, doc){
-    var search = null;
+// Zoomerang.js
+;(function(doc, win){
+    if(!doc.querySelectorAll) {
+        return
+    }
+    Zoomerang.config({
+        transitionDuration: '.3s',
+        bgOpacity: 1,
+        maxWidth: 1000,
+        maxHeight: 1000
+    })
+    Zoomerang.listen('.j-zoomerage-range img')
+})(document, window)
+
+;(function(win, doc){
+    var search = null
     doc.addEventListener('DOMContentLoaded', function(){
-        search = new win.o.Search();
-    });
-})(window, document);
+        search = new win.o.Search()
+    })
+})(window, document)
